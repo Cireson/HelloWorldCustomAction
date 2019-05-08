dcdfcded3267bb834d859a404660b9ad8be58ec3
Andrey Arias <andrey.arias@cireson.com>
2019-05-08 13:15:27 -0700

    Add project files.

---
+++ HelloWorldCustomAction/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HelloWorldCustomAction")]
+[assembly: AssemblyDescription("HelloWorldCustomAction")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cireson")]
+[assembly: AssemblyProduct("HelloWorldCustomAction")]
+[assembly: AssemblyCopyright("Copyright � Cireson 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a2948c07-9628-4412-b0be-97cf54d01a19")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.62.69")]
+
+[assembly: AssemblyInformationalVersion("1.0.62-RC.69+Branch.integration.Sha.4dbd9a76e969a310655066babf49924f3185b19b")]
+[assembly: AssemblyFileVersion("1.0.62.0")]
